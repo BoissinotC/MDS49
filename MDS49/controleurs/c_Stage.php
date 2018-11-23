c808a45e185275bb8076bc6cc532233bbf1e1821
Paul-Roussel <43873086+Paul-Roussel@users.noreply.github.com>
2018-11-23 11:27:52 +0100

    Add files via upload

---
+++ MDS49/controleurs/c_Stage.php
@@ -0,0 +1,20 @@
+<?php
+$action = $_REQUEST['action'];
+switch($action)
+{
+	case 'demandeStage':
+	{
+		include("vues/v_demandeStage.php");
+		break;
+	}
+	case 'confirmerStage':
+	{
+		
+		break;
+	}
+}
+
+
+?>
+
+
